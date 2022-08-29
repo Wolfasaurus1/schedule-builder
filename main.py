9597d0c50c9b05fce8bc8a443ee31ad3e9190abb
Josh <thewolfinators@gmail.com>
2022-08-29 18:00:00 -0400

    dsxd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
 
 
-resp = requests.get("https://front40.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&lang=en", headers={'origin': 'https://www.omegle.com/', 'referer': "https://www.omegle.com/", 'host': "https://www.omegle.com/"})
+resp = requests.get("https://front40.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&lang=en", headers={'origin': 'https://www.omegle.com/', 'referer': "https://www.omegle.com/"})
 print(resp.text)
