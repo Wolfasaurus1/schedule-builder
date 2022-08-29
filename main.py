538b51a6a921d61c5fe12c6172696d18efd7267e
Josh <thewolfinators@gmail.com>
2022-08-29 18:04:38 -0400

    fixing stuff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
 
-
-resp = requests.post("https://front40.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&lang=en", headers={'origin': 'https://www.omegle.com/', 'referer': "https://www.omegle.com/"})
-print(resp.text)
+resp = requests.get("https://front40.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&lang=en", headers={'origin': 'https://www.omegle.com/', 'referer': "https://www.omegle.com/"})
+print(resp.request.headers)
