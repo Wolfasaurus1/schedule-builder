db39b0e756d7469363724436dccf827320168c78
Josh <thewolfinators@gmail.com>
2022-08-29 17:58:21 -0400

    more change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
 
-resp = requests.get("https://front40.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&lang=en")
+
+resp = requests.get("https://front40.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&lang=en", headers={'origin': 'https://www.omegle.com/', 'referer': "https://www.omegle.com/", 'host': "https://www.omegle.com/"})
 print(resp.text)
