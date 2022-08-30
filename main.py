3eab8e1ccdb8ce1287db94c28127fb61aba01686
Josh <thewolfinators@gmail.com>
2022-08-29 20:35:53 -0400

    stuff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-resp = requests.get("https://front40.omegle.com/start?caps=recaptcha2,t&firstevents=1&spid=&lang=en", headers={'origin': 'https://www.omegle.com/', 'referer': "https://www.omegle.com/"})
-print(resp.request.headers)
+resp = requests.get("https://postman-echo.com/headers", headers={'origin': 'https://www.omegle.com/', 'referer': "https://www.omegle.com/"})
+print(resp.text)
 
 #stuff
